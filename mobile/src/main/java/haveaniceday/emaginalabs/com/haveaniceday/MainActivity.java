7bf7fbe14ecf3e7db89395d69a220afd19195260
Sergio Arroyo <sergio.arroyo.cuevas@gmail.com>
2017-09-16 16:46:19 +0200

    just create project with android studio

---
+++ mobile/src/main/java/haveaniceday/emaginalabs/com/haveaniceday/MainActivity.java
@@ -0,0 +1,13 @@
+package haveaniceday.emaginalabs.com.haveaniceday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
