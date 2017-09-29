d8fd1222cc4a045ad8788737bdf1213857a90937
Sergio Arroyo <sergio.arroyo.cuevas@gmail.com>
2017-09-29 19:05:12 +0200

    Show big image style in notification

--- mobile/src/main/kotlin/haveaniceday/emaginalabs/com/haveaniceday/notification/HappyNotificationMessagingService.kt
+++ mobile/src/main/kotlin/haveaniceday/emaginalabs/com/haveaniceday/notification/HappyNotificationMessagingService.kt
@@ -4,8 +4,8 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.support.v4.app.NotificationCompat
-import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import haveaniceday.emaginalabs.com.haveaniceday.R
@@ -16,9 +16,6 @@ object HappyNotificationMessaging {
 }
 
 class HappyNotificationMessagingService : FirebaseMessagingService() {
-
-    val TAG = "HappyNotifidationMsg"
-
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         val title = remoteMessage.data.get("title")
@@ -34,12 +31,18 @@ class HappyNotificationMessagingService : FirebaseMessagingService() {
         intent.putExtra(HappyNotificationMessaging.RECEIVED_MESSAGE, notification.message)
         val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
 
+
+        val bigPictureStyle = NotificationCompat.BigPictureStyle()
+                .bigPicture(BitmapFactory.decodeResource(this.getResources(), R.mipmap.notification_background))
+                .setSummaryText(notification.message)
+
         val notificationBuilder = NotificationCompat.Builder(this)
                 .setSmallIcon(R.mipmap.ic_notify)
                 .setContentTitle(notification.title)
                 .setContentText(notification.message)
                 .setAutoCancel(true)
                 .setContentIntent(pendingIntent)
+                .setStyle(bigPictureStyle)
 
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
