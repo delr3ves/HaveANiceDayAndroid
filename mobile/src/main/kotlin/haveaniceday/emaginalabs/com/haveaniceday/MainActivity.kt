1f90cb0b66720df186f6464ae14b6cc2fb1944f9
Sergio Arroyo <sergio.arroyo.cuevas@gmail.com>
2017-10-02 09:15:37 +0200

    Configure crashlytics

--- mobile/src/main/kotlin/haveaniceday/emaginalabs/com/haveaniceday/MainActivity.kt
+++ mobile/src/main/kotlin/haveaniceday/emaginalabs/com/haveaniceday/MainActivity.kt
@@ -2,11 +2,15 @@ package haveaniceday.emaginalabs.com.haveaniceday
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
         setContentView(R.layout.activity_main)
     }
 }
